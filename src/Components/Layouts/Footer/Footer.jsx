adf33a9c33e38f877978f91eb338348c2a35b7c5
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-01 22:30:11 +0200

    bug fix in footer components

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import footerStyle from "./Footer.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 function Footer() {
   return (
     <div className={footerStyle.footer}>
-      <p>This a Clone of Dominos Website, 2023 Dominos Pizza. All Rights Reserved</p>
+      <p className={footerStyle.text}>This a Clone of Dominos Website, 2023 Dominos Pizza. All Rights Reserved</p>
       <div className={footerStyle.icons}>
-        <NavLink>
+        <Link className={footerStyle.link}>
           <i class="bi bi-instagram"></i>
-        </NavLink>
-        <NavLink>
+        </Link>
+        <Link className={footerStyle.link}>
           <i class="bi bi-facebook"></i>
-        </NavLink>
+        </Link>
       </div>
       <div></div>
     </div>
